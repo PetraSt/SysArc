7642c22fc011ae97b55e7fe726c4692275b770b4
dprzygocka <przygocka.dagmara@gmail.com>
2023-03-23 18:48:29 +0100

    add log

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ let startTime
 let count = 0
 
 app.post('/data', (req, res) => {
-  console.log(req.body)
   // do something with the data, e.g. store it in a database
   count++
   if (count === 1) {
@@ -18,6 +17,7 @@ app.post('/data', (req, res) => {
     const endTime = Date.now()
     const totalTime = (endTime - startTime) / 1000
     console.log(`Received 1000 messages in ${totalTime} s`)
+    console.log(`Message delivery rate: ${delivery_rate} messages/second`)
   }
   res.status(200).send('Data received')
 })
